d4140fa00776dc14cdc36e832542bf5910bf3088
axl-art <axeljamilarteaga26@gmail.com>
2021-04-11 23:40:35 -0500

    Added solution to problem 009

---
+++ Problems/problem_009.f90
@@ -0,0 +1,34 @@
+PROGRAM problem_009
+    
+	IMPLICIT NONE
+ 	
+ 	INTEGER :: a = 0, b = 0, c = 0
+	INTEGER :: prod = 1
+
+  DO a = 1, 1000
+    
+    IF ( MOD(1000*(-500 + a), (-1000 + a)) .EQ. 0 ) THEN
+
+      b = 1000*(-500+a) / (-1000 + a)
+      c = 1000 - (a + b)
+
+      WRITE (*,*) "a: ", a
+      WRITE (*,*) "b: ", b
+      WRITE (*,*) "c: ", c
+
+      IF ((a + b + c) .EQ. 1000) THEN
+
+        WRITE (*,*) "Got!"
+       
+        prod = a * b * c
+       
+        WRITE (*,*) "the product is: ", prod
+
+        STOP
+
+      END IF
+
+    END IF
+  END DO     
+
+END PROGRAM
